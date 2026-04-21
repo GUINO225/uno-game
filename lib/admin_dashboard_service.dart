e060a1e43da1b9964d83a2a68228c8e9f7b31776
GUINO225 <stephanegnahoua892@gmail.com>
2026-04-21 22:46:16 +0000

    Allow akenoostudio Gmail for admin dashboard access

--- lib/admin_dashboard_service.dart
+++ lib/admin_dashboard_service.dart
@@ -58,6 +58,7 @@ class AdminDashboardService {
 
   static const Set<String> _fallbackAllowedAdminEmails = <String>{
     'admin@uno-game.local',
+    'akenoostudio@gmail.com',
   };
 
   CollectionReference<Map<String, dynamic>> get _profiles =>
