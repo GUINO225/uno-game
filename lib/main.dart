496cde0477759611b2cb80b403132d79d808a544
GUINO225 <stephanegnahoua892@gmail.com>
2026-04-28 21:23:35 +0000

    Fix missing kIsWeb import in Flutter entrypoint

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
